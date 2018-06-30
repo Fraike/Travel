6ec7873a8ddd0565e3bbe0009bb791b1d06a9572
wwt <648128145@qq.com>
2018-06-30 14:17:19 +0800

    swiper

--- README.md
+++ README.md
@@ -19,3 +19,14 @@ npm run build --report
 ```
 
 For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
+
+<!-- #安装fastclick -->
+npm install fastclick --save
+
+<!-- 安装stylus -->
+npm install stylus --save
+npm install stylus-loader --save
+
+<!-- 添加iconfont -->
+
+<!-- 安装vue-awesome-swiper -->
---
+++ src/pages/home/components/swiper.vue
@@ -0,0 +1,48 @@
+<template>
+<div class="wrapper">
+    <swiper :options="swiperOption">
+    <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl" alt="">
+    </swiper-slide>
+    <div class="swiper-pagination"  slot="pagination"></div>
+  </swiper>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HomeSwiper',
+    data () {
+        return {
+            swiperOption: {
+                pagination: '.swiper-pagination',
+                loop: true
+            },
+            swiperList: [{
+                id:'0001',
+                imgUrl:'http://img1.qunarzz.com/piao/fusion/1806/fc/e47aa3e1c67bbc02.jpg_750x200_0f3eecf8.jpg'
+            },
+            {
+                id:'0002',
+                imgUrl: 'http://img1.qunarzz.com/piao/fusion/1804/bd/8e4a1c3f470d3702.jpg_750x200_f1f0a8c7.jpg'
+            },
+            ]
+        }
+    }
+}
+</script>
+
+<style lang="stylus" scoped>
+    .wrapper >>> .swiper-pagination-bullet-active
+        background: #fff
+    .wrapper
+        width: 100%;
+        height: 0;
+        overflow: hidden
+        padding-bottom: 31.25%
+        backgroung: #eee
+        .swiper-img 
+            width: 100%
+</style>
+
+
